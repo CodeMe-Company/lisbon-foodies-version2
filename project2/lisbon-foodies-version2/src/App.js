6aba7a6fff88fd1fc6eb204313b9cd6214d09cfc
mzakrzewska1 <120480139+mzmoonsun@users.noreply.github.com>
2023-03-31 17:06:25 +0100

    adress footer issues

--- project2/lisbon-foodies-version2/src/App.js
+++ project2/lisbon-foodies-version2/src/App.js
@@ -4,6 +4,7 @@ import {Routes, Route} from "react-router-dom"
 import NavBar from './Components/NavBar/NavBar';
 import HomePage from './Components/HomePage/HomePage';
 import RestaurantList from "./Components/RestaurantList/RestaurantList";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/restaurant-list" element={<RestaurantList />} />
       </Routes> 
+      <Footer />
       
   
     </div>
