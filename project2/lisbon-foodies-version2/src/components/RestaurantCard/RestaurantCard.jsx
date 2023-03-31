22838aa8e219add708b38891ea82c5b7b33b53e3
Filipe Carneiro <filipemanuelcarneiro@gmail.com>
2023-03-31 10:33:36 +0000

    Added the SearchBar, RestaurantCard and RestaurantList and did the Route thing in the App

---
+++ project2/lisbon-foodies-version2/src/components/RestaurantCard/RestaurantCard.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./RestaurantCard.css";
+
+
+const RestaurantCard = ({restaurant}) => {
+
+  return (
+    <div key={restaurant.id}>
+      
+      <p>Name: {restaurant.name}</p>
+      <p>Location: {restaurant.location.address1}</p>
+      <p>Phone: {restaurant.phone}</p>
+      <p>Rating: {restaurant.rating}</p>
+      <img src={restaurant.image_url} alt={restaurant.name} className="image"/>
+    </div>
+  )
+}
+
+export default RestaurantCard
