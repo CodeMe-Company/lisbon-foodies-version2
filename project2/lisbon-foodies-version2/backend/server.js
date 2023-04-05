cfac43ff91fd381af8c22d55de797e321ecd7b1f
Filipe Carneiro <filipemanuelcarneiro@gmail.com>
2023-04-05 10:25:45 +0000

    Added the event list, event card and CSS files for each one

--- project2/lisbon-foodies-version2/backend/server.js
+++ project2/lisbon-foodies-version2/backend/server.js
@@ -72,3 +72,26 @@ app.get("/restaurant-reviews/:id", (req, res) => {
 app.listen(8080, (req, res) => {
   console.log("listening on port 8080");
 });
+
+
+//*get event list
+app.get("/event-list", (req, res) => {
+  const options = {
+    headers: {
+      accept: "application/json",
+      "Access-Control-Allow-Origin": "*",
+      Authorization:
+        "Bearer LhgMafKGB-TTN1QsQ9rvNM7BMio765gCO_4fVoP71OIL8STkFNb1C_pNdwpRBQhtmLRXJ3C3JRd9Kz0AR3ehQjhNo-x0p2zXMrfTsjJA_qLjr0UycN9ShC2imogdZHYx",
+    },
+  };
+  axios
+    .get(
+      "https://api.yelp.com/v3/events?limit=50&sort_by=desc&sort_on=popularity&excluded_events=&location=portugal&categories=food-and-drink",
+      options
+    )
+    .then((response) => {
+      res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+      res.send(response.data.events);
+    })
+    .catch((error) => console.error(error));
+});
